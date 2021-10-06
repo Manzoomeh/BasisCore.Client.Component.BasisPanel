fc3cce78e8533164b8f5622a5de0c0bde4120ac6
Qamsari <ali.qamsari@gmail.com>
2021-10-06 11:16:54 +0330

    Editing TaskProcess

--- src/components/scheduler/TaskProcess.ts
+++ src/components/scheduler/TaskProcess.ts
@@ -1,11 +1,20 @@
 import ITaskOptions from "./ITaskOptions";
 import SchedulerComponent from "./SchedulerComponent";
 
-export default  class TaskProcess {
+export default class TaskProcess {
     private _owner: SchedulerComponent;
     private _options: ITaskOptions;
+    public get id(): number {
+        return this._options.id;
+    }
+
     constructor(owner: SchedulerComponent, options: ITaskOptions) {
         this._owner = owner;
         this._options = options;
+        this._options.task.then(x => this.dispose.bind(this));
+    }
+
+    public dispose(): void {
+        this._owner.processEnded(this);
     }
 }
