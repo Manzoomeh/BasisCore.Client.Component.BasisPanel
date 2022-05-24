925176ec327ee03163e069b76cf2f96a49bebf38
Zahra Akbari <akbari.z1002@gmail.com>
2022-05-24 11:05:55 +0430

    Adding svg type to webpack.config for embedding in js file

--- webpack.config.js
+++ webpack.config.js
@@ -86,10 +86,14 @@ module.exports = (env, options) => {
           test: /\.html$/i,
           type: "asset/source",
         },
+        {
+          test: /\.svg$/i,
+          type: "asset/inline",
+        },
       ],
     },
     resolve: {
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".css", ".png", ".html"], // there's a dot missing
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".css", ".png", ".html", ".svg"], // there's a dot missing
     },
     plugins: [
       new CircularDependencyPlugin({
