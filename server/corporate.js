a48f61fd3b6200350fd16716ba443b87ceb8b156
Qadireh Moradi <kmg.d117@gmail.com>
2023-01-10 15:08:50 +0330

    edit style in user level

--- server/corporate.js
+++ server/corporate.js
@@ -6,10 +6,66 @@ const { active } = require("./active-manager");
 
 const corporateList = [
   {
+    id: 8956,
+    title: "Avasys",
+  },
+  {
+    id: 20,
+    title: "BasisDocs",
+  },
+  {
+    id: 8844,
+    title: "BHK",
+  },
+  {
     id: 7660,
     title: "Bisko",
   },
-];
+  {
+    id: 8935,
+    title: "BROTHER",
+  },
+  {
+    id: 7,
+    title: "manzoomehnet.ir",
+  },
+  {
+    id: 5,
+    title: "Seller.ir",
+  },
+  {
+    id: 8327,
+    title: "آبا-شیمی",
+  },
+  {
+    id: 8898,
+    title: "آبینه-سیر-آبشار",
+  },
+  {
+    id: 8478,
+    title: "آرت-تکنیکال",
+  },
+  {
+    id: 1,
+    title: "منظومه نگاران",
+  },
+  {
+    id: 8625,
+    title: "مهرسان-نوین",
+  },
+  {
+    id: 8282,
+    title: "مهرستا",
+  },
+  {
+    id: 8479,
+    title: "مکش-و-دهش",
+  },
+  {
+    id: 8355,
+    title: "میترا-گشت",
+  },
+]
 router.get("/:rKey/list", function (req, res) {
   res.json(corporateList);
 });
