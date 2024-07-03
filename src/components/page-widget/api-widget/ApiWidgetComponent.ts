94792347b0ef78443158ff9e285e4e7e087301b2
1senka <sina13kia@gmail.com>
2024-07-03 16:45:14 +0330

    remove unused imports

--- src/components/page-widget/api-widget/ApiWidgetComponent.ts
+++ src/components/page-widget/api-widget/ApiWidgetComponent.ts
@@ -1,17 +1,6 @@
-import desktopLayout from "./assets/layout-desktop.html";
-import mobileLayout from "./assets/layout-mobile.html";
-import "./assets/style.css";
-import "./assets/style-desktop.css";
-import "./assets/style-mobile.css";
 import HttpUtil from "../../../HttpUtil";
 import { IUserDefineComponent } from "basiscore";
-import { ISource } from "basiscore";
-import PageWidgetComponent from "../PageWidgetComponent";
-import IPage from "../../page/IPage";
-import NotificationMessageComponent from "../../notificationMessage/NotificationMessageComponent";
-import WidgetListComponent from "../../widget-list/WidgetListComponent";
 import MenuComponent from "../../menu/MenuComponent";
-import PageComponent from "../../page/PageComponent";
 import WidgetComponent from "../widget/WidgetComponent";
 
 export default class ApiWidgetComponent extends WidgetComponent {
